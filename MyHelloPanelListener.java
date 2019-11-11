1c3135b83261783bde6002f336a6d25b720fbd59
jch1999 <elvpfhhrm@gmail,com>
2019-11-11 12:49:06 +0900

    클래스 수정 MyHelloListener -> MyHelloPanelListener

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -1,19 +1,19 @@
 
 /**
- * 여기에 MyHelloListener 클래스 설명을 작성하십시오.
+ * 여기에 MyHelloPanelListener 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloListener
+public class MyHelloPanelListener
 {
     // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
     private int x;
 
     /**
-     * MyHelloListener 클래스의 객체 생성자
+     * MyHelloPanelListener 클래스의 객체 생성자
      */
-    public MyHelloListener()
+    public MyHelloPanelListener()
     {
         // 인스턴스 변수의 초기화
         x = 0;
