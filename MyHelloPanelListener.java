5b016880f4ef425bdc5b1612a55201df61d6d4b3
jch1999 <elvpfhhrm@gmail,com>
2019-11-11 12:58:00 +0900

    리스너 클래스 작성중 도중 저장

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -1,11 +1,22 @@
-
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * 여기에 MyHelloPanelListener 클래스 설명을 작성하십시오.
  * 
- * @author (2018315034 정철우, 2018243087 정찬우) 
+ * @author (2018315034 정철우, 2018243087 정찬호) 
  * @version (2019/11/11)
  */
-public class MyHelloPanelListener
+public class MyHelloPanelListener extends JPanel implements MouseListener, KeyListener
 {
-   
+    public MyHelloPanelListener(){
+        
+    }
+    public void mousePressed(MouseEvent e){}
+    public void mouseClicked(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    public void mouseEnterd(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
+    public void keyPressed(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
 }
