02be9f85fb6fcfa78257d65c75cc29eb1ab69de1
JEONGCHEOLWOO <tonyprince1228@gmail.com>
2019-11-11 12:50:45 +0900

    학번, 날짜 기입

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -2,8 +2,8 @@
 /**
  * 여기에 MyHelloFrame 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2018315034 정철우, 2018243087 정찬우) 
+ * @version (2019/11/11)
  */
 public class MyHelloFrame
 {
